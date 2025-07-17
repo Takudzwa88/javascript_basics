a21c05c1e8773f53643e25481a52c245cbf0ef43
Takudzwa88 <kanokangatakudzwa@gmail.com>
2025-07-17 11:15:48 +0200

    Javascript introduction

---
+++ javascript_basics.js
@@ -0,0 +1,56 @@
+let name = "Takudzwa";
+console.log(name);
+
+const age = 25;
+console.log(age);
+
+let isStudent = true;
+console.log(isStudent);
+
+let city = null;
+console.log(city);
+
+let country = undefined;
+console.log(country);
+
+let fruits = ["apple", "banana", "cherry"];
+console.log(fruits);
+
+let items = ["porsche", 21, true, null];
+console.log(items);
+
+const things = ["porsche", 21, true, null];
+console.log(things);
+
+const person = {
+  name: "Tariro",
+  surname: "Thole",
+  age: 30,
+  location: "Harare",
+  gender: "female",
+  hobbies: ["reading", "traveling", "cooking"],
+};
+console.log(person);
+
+let colors = ["red", "green", "blue", "yellow", "purple", "orange"];
+console.log(colors[0]);
+console.log(colors[2]);
+colors.push("maroon");
+console.log(colors);
+colors.pop();
+console.log(colors);
+colors.shift();
+console.log(colors);
+colors.unshift("pink");
+console.log(colors);
+colors.length;
+console.log(colors.length);
+colors.forEach((b) => {
+    console.log(b);
+});
+
+colors.map((b) => {
+    console.log(b);
+});
+
+
